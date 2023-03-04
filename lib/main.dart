f373956155be121c04cbf48fe48409ebb15fdc3d
mmzk1526 <yc4120@ic.ac.uk>
2023-03-04 21:47:18 +0000

    [mmzk] (feat) Change names properly

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Register Machine Simulator',
       theme: theme,
       home: const HomePage(),
     );
